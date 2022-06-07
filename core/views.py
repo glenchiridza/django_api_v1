677b3ebcf25889f1ba8e8a56acb6ebd544cc716e
glenchiridza <glenchiridz@gmail.com>
2022-06-07 11:24:33 +0200

    rest framework

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 def test_view(request):
-    data ={
-        'name':'glen',
-        'age':23
+    data = {
+        'name': 'glen',
+        'age': 23
     }
     return JsonResponse(data)
+
+
+class TestView(APIView):
+    def get(self,request,*args,**kwargs):
+        data = {
+            'name': 'glen',
+            'age': 23
+        }
+        return Response(data)
--- dresrf_api/urls.py
+++ dresrf_api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import test_view
+from core.views import test_view,TestView
+from django.urls import include
 
 urlpatterns = [
+    path('api-auth',include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('',test_view,name='test')
+    path('test',test_view,name='test'),
+    path('',TestView.as_view(),name='testview')
 ]

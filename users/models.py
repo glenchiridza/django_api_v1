376ffe969f6293ec611eadb3291ae1d2e041425f
glenchiridza <glenchiridz@gmail.com>
2022-06-16 21:09:26 +0200

    student model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='profile_images', max_length=255, blank=True, null=True)
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Collaborator(models.Model):
+

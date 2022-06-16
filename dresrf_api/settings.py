2b6a4b25c96f12b81f6199976971845b1b0aa202
glenchiridza <glenchiridz@gmail.com>
2022-06-16 22:50:33 +0200

    update

--- dresrf_api/settings.py
+++ dresrf_api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h%j^rww3i$--n0nygx+#77rw2x+8#z64(&z65_qwm*%85n@i0^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

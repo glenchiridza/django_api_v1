95fd08fea7bf20104603a5904faffad1539b08f9
glenchiridza <glenchiridz@gmail.com>
2022-06-22 21:55:30 +0200

    lader board

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 User = get_user_model()
 
 
@@ -12,3 +13,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class LeaderBoard(models.Model):
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    count = models.IntegerField()
+
+    @property
+    def update_leader_board(self):
+        url = reverse('')
+        a = '''<a href="{}">Update leader board</a>'''
+        return mark_safe(a)
+
+    def __str__(self):
+        return f'contributions by {self.contributor}'

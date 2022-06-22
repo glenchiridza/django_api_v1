dc833ecef3a868b49af67245e89938d1bebc6d58
glenchiridza <glenchiridz@gmail.com>
2022-06-22 21:58:11 +0200

    customize admin format

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, LeaderBoard
 
 admin.site.register(Post)
+
+
+@admin.register(LeaderBoard)
+class LeaderBoard2(admin.ModelAdmin):
+    list_display = ("contributor", "department", "count", "update_leader_board")
